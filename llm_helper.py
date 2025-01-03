7d81120724c8ed04e2549780f5be4cc0536ab458
RexO77 <nischal.skanda07@gmail.com>
2025-01-03 14:15:53 +0530

    New feature

--- llm_helper.py
+++ llm_helper.py
@@ -7,4 +7,4 @@ llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b
 if __name__ == "__main__":
     response = llm.invoke("Two most important ingradient in samosa are ")
     print(response.content)
-
+#llm check to see if the api key works
