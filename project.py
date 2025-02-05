217e413093fb0b5753fe333c9674aa63b4000d07
rexO77 <nischal.skanda07@gmail.com>
2025-02-05 18:28:32 +0530

    upgrade

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Posty - AI Post Generator",
     page_icon="✨",
     layout="wide",
-    initial_sidebar_state=""
+    initial_sidebar_state="collapsed"
 )
 
 # Apply theme
