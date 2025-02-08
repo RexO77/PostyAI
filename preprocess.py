81b470d3cb3757cfdae425b276e3eca53b66f78d
rexO77 <nischal.skanda07@gmail.com>
2025-02-08 22:17:37 +0530

    upgrade

--- preprocess.py
+++ preprocess.py
@@ -84,4 +84,4 @@ def get_unified_tags(posts_with_metadata):
 if __name__ == "__main__":
     process_posts("data/raw_posts.json", "data/processed_posts.json")
 
-#havent installed langchain_core yet
+
