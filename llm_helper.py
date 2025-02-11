2b785c610e757c3205e937f7372b6434ce4db95d
rexO77 <nischal.skanda07@gmail.com>
2025-02-11 20:35:26 +0530

    IDK

--- llm_helper.py
+++ llm_helper.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="deepseek-r1-distill-llama-70b")
 if __name__ == "__main__":
     response = llm.invoke("Two most important ingradient in samosa are ")
     print(response.content)
+
